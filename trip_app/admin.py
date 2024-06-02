267dd834a60f918ffba20f2bbd82c0afc65a94e8
Edward Litvinchuck <elitvinchuck@gmail.com>
2024-06-02 18:52:18 +0300

    Models and admin mapping

--- trip_app/admin.py
+++ trip_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from trip_app.models import Trip, Review, Destination, Friend
+
 # Register your models here.
+admin.site.register(Trip)
+admin.site.register(Review)
+admin.site.register(Destination)
+admin.site.register(Friend)
--- trip_app/models.py
+++ trip_app/models.py
@@ -3,11 +3,40 @@ from django.contrib.auth.models import User
 
 # Використовувати Postgres для зберігання користувачів (id, name, email, password, preferences), '
 # подорожей (id, user_id, trip_name, start_date, end_date, destination), 
-# друзів (user_id, friend_id) та відгуків (id, user_id, destination, rating)
+# друзів (user_id, friend_id) та 
+# відгуків (id, user_id, destination, rating)
+
 
 class Trip(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     trip_name = models.CharField(max_length=100)
     start_date = models.DateField() 
     end_date = models.DateField()
-    destination = models.CharField(max_length=50)
+    destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Trip({self.trip_name}, {self.start_date}, {self.end_date})"
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
+    rating = models.SmallIntegerField()
+
+    def __str__(self) -> str:
+        return f"Review({self.user}, {self.destination}, {self.rating})"
+
+
+class Destination(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"Destination({self.name})"
+    
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_of')
+
+    def __str__(self) -> str:
+        return f"The {self.user} is friend of {self.friend}"
